7fd49b4c2f31dd0da68f10eebce007f2bb2c1fa4
Caio Stato <zombieiron9@gmail.com>
2021-07-01 12:38:29 -0300

    remake css + leitura noticias firebase

---
+++ public/scripts/app.js
@@ -0,0 +1,48 @@
+
+function load(event) {
+    event.preventDefault();
+
+    let titles = new Array();
+    let str = new Array();
+    let count = 0;
+    let x = document.getElementsByClassName('noticiaPrincipal');
+    
+    db.collection("noticias").get()
+    .then((querySnapshot) => {
+        // Leituta noticias
+        querySnapshot.forEach((doc) => {
+            
+            str[count] = "background: url("+ doc.data().img+") center / cover no-repeat;";
+            titles[count] = doc.data().titulo;
+            
+            count++;
+        });
+        
+        // Sorteio de noticias
+        for (let i = 0; i < x.length; i++) {
+            let num = getRandomInt(0,str.length);
+
+            // Uso DOM
+            x[i].setAttribute("style",str[num]);
+            document.getElementById("idNotTitle"+(i)).textContent = titles[num];
+            
+            str.splice(num,1);
+            titles.splice(num,1);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
+// Curtir noticias 
+function like() {
+    console.log("oi");
+}
+
+// Randomizacao
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
---
+++ scripts/loginRegister.js
@@ -0,0 +1,42 @@
+function authenticateUser(event){
+    event.preventDefault();
+    let email = document.getElementById("loginId").value;
+    let senha = document.getElementById("loginPassword").value;
+    firebase.auth().signInWithEmailAndPassword(email,senha)
+    .then(function(){
+        console.log("Usuario logado com sucesso");
+        user = firebase.auth().currentUser;
+        
+        localStorage.setItem('username',user.uid);
+    })
+    .catch(function(error){
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+
+        alert("ERRO");
+    })
+}
+
+function registerPage() {
+    window.open("/remake/htmls/register.html");
+}
+
+function createUser(event){
+    event.preventDefault();
+    let email = document.getElementById("registerId").value;
+    let senha = document.getElementById("registerPassword").value;
+    firebase.auth().createUserWithEmailAndPassword(email, senha)
+    .then(function(){
+        console.log("Usuario criado com sucesso!");
+        alert("Usuario criado com sucesso!");
+    })
+    .catch(function(error){
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    })
+}
+//loggoff
